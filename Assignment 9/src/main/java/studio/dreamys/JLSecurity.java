09cfd816d6e0c0104499cbd7150bb7e1ad572a3d
dxxxxy <introsdream@gmail.com>
2022-12-02 10:44:01 -0500

    impl: do loginCount & fixes

--- Assignment 9/src/main/java/studio/dreamys/JLSecurity.java
+++ Assignment 9/src/main/java/studio/dreamys/JLSecurity.java
@@ -21,12 +21,20 @@ public class JLSecurity implements IJLSecurity {
 
     @Override
     public void CreateUser(String user, String password) {
+        //init
         byte[] hash;
-        byte[] salt;
+        byte[] salt = new byte[16];
 
+        //check if user already exists
+        IUser dbUser = getUser(user);
+        if (dbUser != null) {
+            System.out.println("This user already exists. Please log in.");
+            return;
+        }
+
+        //register non-existing user
         try {
             //create secure salt
-            salt = new byte[16];
             SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
             sr.nextBytes(salt);
 
@@ -37,52 +45,69 @@ public class JLSecurity implements IJLSecurity {
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
             hash = skf.generateSecret(spec).getEncoded();
 
-            //update db (add, if already added: update)
+            //update db (add new user)
             UpdateDB(new User(user, salt, hash, 0));
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            e.printStackTrace();
-            System.out.println("Error creating user");
+            System.out.println("There was an error while hashing your password. Please try again.");
         }
     }
 
     @Override
     public boolean Login(String user, String password) {
+        //check if user exists
         IUser dbUser = getUser(user);
+        if (dbUser == null) {
+            System.out.println("This user was not found in the database. Please create a new user.");
+            return false;
+        }
 
+        //check if failedLoginCount > 5
+        if (dbUser.getFailedLoginCount() > 4) {
+            System.out.println("This user has been locked out due to too many failed login attempts.");
+            return false;
+        }
+
+        //init
         byte[] salt = dbUser.getSalt();
         byte[] hash = dbUser.getHash();
         byte[] check_hash;
 
         //create secure keyspec
-        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 2, hash.length * 8);
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 5, hash.length * 8);
 
         try {
             //create hash
             SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
             check_hash = skf.generateSecret(spec).getEncoded();
         } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            e.printStackTrace();
-            System.out.println("Error logging in");
+            System.out.println("There was an error while hashing your password. Please try again.");
             return false;
         }
 
+        boolean same = Arrays.equals(hash, check_hash);
+
+        if (!same) {
+            //update existing user
+            System.out.println("Incorrect password. Please try again.");
+            dbUser.setFailedLoginCount(dbUser.getFailedLoginCount() + 1);
+            System.out.println("Failed login count: " + dbUser.getFailedLoginCount());
+            UpdateDB(dbUser);
+        }
+
         //check if hashes match
-        return Arrays.equals(check_hash, hash);
+        return same;
     }
 
     private IUser getUser(String username) {
         IUser user = null;
 
         try (CallableStatement cs = conn.prepareCall("{ ? = call GET_USER(?) }")) {
-            cs.registerOutParameter(1, Types.STRUCT);
+            cs.registerOutParameter(1, Types.STRUCT, User.SQL_TYPE_NAME);
             cs.setString(2, username);
             cs.execute();
 
             user = (IUser) cs.getObject(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Error getting user");
-        }
+        } catch (SQLException ignored) {}
 
         return user;
     }
@@ -91,9 +116,10 @@ public class JLSecurity implements IJLSecurity {
         try (CallableStatement cs = conn.prepareCall("{ call ADD_USER(?)}")) {
             cs.setObject(1, user);
             cs.execute();
+
+            System.out.println("The database was successfully updated.");
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Error creating user");
+            System.out.println("There was an error while updating the database. Please try again.");
         }
     }
 }
